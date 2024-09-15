5e383b657b985cd77e279ce85dfd4939972ad6ac
Colin Hammarberg <colin.hammarberg@ecomero.com>
2024-09-15 18:46:12 +0200

    initialize backend

---
+++ andnings_apoteket_backend/index.js
@@ -0,0 +1,17 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// A simple route
+app.get('/', (req, res) => {
+  res.send('Hello, Andningsapoteket!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
